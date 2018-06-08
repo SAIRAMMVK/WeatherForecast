a4aef06babacbaec486a2938551756abcd473dd4
Prabhu S <prabhu@virtusa.com>
2018-06-08 15:38:45 +0530

    Updated contents

--- js/weatherfcast.js
+++ js/weatherfcast.js
@@ -7,6 +7,18 @@ function checkInput(){
         $("#btn").prop("disabled",true);    
     }
 }
+var value,newvalue;
+function convertC(){
+    
+    newval = Math.ceil((newvalue -32) * 0.5556) ;
+    $("#tempvalue").html(newval);
+
+}
+function convertF(){
+    newvalue = Math.ceil(value * 1.8 +32) ;
+    //alert(value);
+    $("#tempvalue").html(newvalue);
+}
 
 $("#btn").click(function ()
 
@@ -14,7 +26,7 @@ $("#btn").click(function ()
         var city = $("#cityName").val();
         $("#cityid").html(city);
         var tmpdes;
-    $.ajax({
+        $.ajax({
 
         type:"GET",
         url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=ef0107f6f8d7d8932a3c0d617ef9b1ec',
@@ -30,7 +42,14 @@ $("#btn").click(function ()
                 console.log("in loop");
                     $("#icon").attr("src","images/haze.png");
             }
+            value = Math.ceil((output.main.temp-273));
             $("#tempvalue").html(Math.ceil((output.main.temp-273)));
+            humidity = output.main.humidity+"%";
+            console.log(humidity);
+            windspeed =  output.wind.speed+"km/h";
+            console.log(windspeed);
+            $("#wind").append(windspeed);
+            $("#humidity").append(humidity);
 
         },
 
@@ -41,6 +60,75 @@ $("#btn").click(function ()
 
         }
     });
+
+    $("#temp1").click(()=>{
+        const city = $("#cityName").val();
+        $.ajax({
+
+            type:"GET",
+            url: `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=ef0107f6f8d7d8932a3c0d617ef9b1ec`,
+            success : (output)=>{
+                
+                console.log(output);
+                currentDate = output.list.map((ele) => moment(ele.dt * 1000).format('h:mm a'));
+                console.log(currentDate);
+                currentTemperature = output.list.map(ele => Math.round(ele.main.temp - 273));
+                console.log(currentTemperature);
+                plotChart(currentTemperature, currentDate);
+            },
+            error : (output)=>{
+                console.log(output);
+            }
+        });
+
+        const plotChart = (tempArr, datesArr) => {
+            Highcharts.chart('chart-container', {
+                chart: {
+                    type: 'spline'
+                },
+                title: {
+                    text: ''
+                },
+                xAxis: {
+                    categories: datesArr
+                },
+                yAxis: {
+                    title: {
+                        text: 'Temperature'
+                    },
+                    labels: {
+                        formatter: function () { return this.value + '°'; }
+                    }
+                },
+                tooltip: {
+                    crosshairs: true,
+                    shared: true
+                },
+                plotOptions: {
+                    spline: {
+                        marker: {
+                            radius: 4,
+                            lineColor: '#666666',
+                            lineWidth: 1
+                        }
+                    }
+                },
+                series: [{
+                    name: cityName,
+                    marker: {
+                        symbol: 'square'
+                    },
+                    data: tempArr
+    
+                }]
+            });
+        }
+
+
+
+
+
+    })
 
     
 })
